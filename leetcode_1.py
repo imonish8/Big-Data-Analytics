a932e5b95d7fdeac32138851c3ce482472b0e634
Monish Nule <115737071+imonish8@users.noreply.github.com>
2024-08-26 17:05:58 +0530

    Create leetcode_1.py

---
+++ leetcode_1.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        sum = 0
+        l = len(nums)
+        for i in range(l):
+            for j in range(1,l):
+                    sum = nums[i] + nums[j]
+                    if(sum == target):
+                        return (i,i+1)
+                        
+
+# https://leetcode.com/problems/two-sum/
+
+# trick is to find a brute force approach and then go by optimasation.
