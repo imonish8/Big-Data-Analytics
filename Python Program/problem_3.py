f935516a975700ff32697e1227f086a56d452aed
Monish Nule <115737071+imonish8@users.noreply.github.com>
2024-08-31 10:23:51 +0530

    Create problem_3.py

---
+++ Python Program/problem_3.py
@@ -0,0 +1,9 @@
+def middle(lst):
+    # TODO
+    new_list = []
+    for i in range(1,len(lst)-1):
+        new_list.append(lst[i])
+    return new_list
+        
+# time Complexity : O(n)
+# space : O(n)
