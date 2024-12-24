f175295d4e7c02bee35fa0117d1fdc4f3c4d4ea1
Monish Nule <imonishnule8@gmail.com>
2024-12-24 12:51:26 +0530

    R Day completed

---
+++ R/day1.r
@@ -0,0 +1,32 @@
+myString <- "Hello, World!"
+
+print ( myString)
+library (MASS)
+data()
+AirPassengers
+v1<-c(1,2,3,4,5)
+v1
+l1=list(c(1,2,3),"Evening",23.5)
+l1
+M = matrix( c('a','a','b','c','b','a'), nrow = 2, ncol = 3, byrow = TRUE)
+print(M)
+a <- array(c('Blue','Rock'),dim = c(3,3,2))
+print(a)
+colors<-factor(c('First','Second','First','Second','Second','First','Second'))
+colors
+
+student <- 	data.frame(
+  name=c("Abhishek","Sreekrishna","Radharani"),
+  gender = c("Male", "Male","Female"), 
+  marks = c(152, 171.5, 165), 
+  
+  Age = c(42,38,26)
+)
+print(student)
+airpassanger <- data.frame(AirPassengers)
+print(airpassanger)
+cars
+
+
+df <- data.frame(iris)
+summary(df)
